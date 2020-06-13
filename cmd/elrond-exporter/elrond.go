a997a581dedcec758162e4db4e85b1b1b1635d3b
LuaxY <yann.guineau@voidmx.net>
2020-06-13 09:06:06 -0400

    Fix metrics prefix

--- cmd/elrond-exporter/elrond.go
+++ cmd/elrond-exporter/elrond.go
@@ -61,75 +61,75 @@ func updateStatistics(node *elrond.Node) error {
 	}
 
 	if statistics.Statistics.LiveTPS != nil {
-		metricsStatistics["erb_stats_live_tps"].WithLabelValues(node.Name).Set(*statistics.Statistics.LiveTPS)
+		metricsStatistics["erd_stats_live_tps"].WithLabelValues(node.Name).Set(*statistics.Statistics.LiveTPS)
 	}
 
 	if statistics.Statistics.PeakTPS != nil {
-		metricsStatistics["erb_stats_peak_tps"].WithLabelValues(node.Name).Set(*statistics.Statistics.PeakTPS)
+		metricsStatistics["erd_stats_peak_tps"].WithLabelValues(node.Name).Set(*statistics.Statistics.PeakTPS)
 	}
 
 	if statistics.Statistics.BlockNumber != nil {
-		metricsStatistics["erb_stats_block_number"].WithLabelValues(node.Name).Set(*statistics.Statistics.BlockNumber)
+		metricsStatistics["erd_stats_block_number"].WithLabelValues(node.Name).Set(*statistics.Statistics.BlockNumber)
 	}
 
 	if statistics.Statistics.RoundNumber != nil {
-		metricsStatistics["erb_stats_round_number"].WithLabelValues(node.Name).Set(*statistics.Statistics.RoundNumber)
+		metricsStatistics["erd_stats_round_number"].WithLabelValues(node.Name).Set(*statistics.Statistics.RoundNumber)
 	}
 
 	if statistics.Statistics.RoundTime != nil {
-		metricsStatistics["erb_stats_round_time"].WithLabelValues(node.Name).Set(*statistics.Statistics.RoundTime)
+		metricsStatistics["erd_stats_round_time"].WithLabelValues(node.Name).Set(*statistics.Statistics.RoundTime)
 	}
 
 	if statistics.Statistics.AverageBlockTxCount != nil {
-		metricsStatistics["erb_stats_average_block_tx_count"].WithLabelValues(node.Name).Set(*statistics.Statistics.AverageBlockTxCount)
+		metricsStatistics["erd_stats_average_block_tx_count"].WithLabelValues(node.Name).Set(*statistics.Statistics.AverageBlockTxCount)
 	}
 
 	if statistics.Statistics.TotalProcessedTxCount != nil {
-		metricsStatistics["erb_stats_total_processed_tx_count"].WithLabelValues(node.Name).Set(*statistics.Statistics.TotalProcessedTxCount)
+		metricsStatistics["erd_stats_total_processed_tx_count"].WithLabelValues(node.Name).Set(*statistics.Statistics.TotalProcessedTxCount)
 	}
 
 	for i, shard := range statistics.Statistics.ShardStatistics {
 		id := strconv.Itoa(i)
 
 		if shard.LiveTPS != nil {
-			metricsStatistics["erb_shard_stats_live_tps"].WithLabelValues(node.Name, id).Set(*shard.LiveTPS)
+			metricsStatistics["erd_shard_stats_live_tps"].WithLabelValues(node.Name, id).Set(*shard.LiveTPS)
 		}
 
 		if shard.AverageTPS != nil {
-			metricsStatistics["erb_shard_stats_average_tps"].WithLabelValues(node.Name, id).Set(*shard.AverageTPS)
+			metricsStatistics["erd_shard_stats_average_tps"].WithLabelValues(node.Name, id).Set(*shard.AverageTPS)
 		}
 
 		if shard.PeakTPS != nil {
-			metricsStatistics["erb_shard_stats_peak_tps"].WithLabelValues(node.Name, id).Set(*shard.PeakTPS)
+			metricsStatistics["erd_shard_stats_peak_tps"].WithLabelValues(node.Name, id).Set(*shard.PeakTPS)
 		}
 
 		if shard.CurrentBlockNonce != nil {
-			metricsStatistics["erb_shard_stats_current_block_nonce"].WithLabelValues(node.Name, id).Set(*shard.CurrentBlockNonce)
+			metricsStatistics["erd_shard_stats_current_block_nonce"].WithLabelValues(node.Name, id).Set(*shard.CurrentBlockNonce)
 		}
 
 		if shard.TotalProcessedTxCount != nil {
-			metricsStatistics["erb_shard_stats_total_processed_tx_count"].WithLabelValues(node.Name, id).Set(*shard.TotalProcessedTxCount)
+			metricsStatistics["erd_shard_stats_total_processed_tx_count"].WithLabelValues(node.Name, id).Set(*shard.TotalProcessedTxCount)
 		}
 
 		if shard.ShardID != nil {
-			metricsStatistics["erb_shard_stats_shard_id"].WithLabelValues(node.Name, id).Set(*shard.ShardID)
+			metricsStatistics["erd_shard_stats_shard_id"].WithLabelValues(node.Name, id).Set(*shard.ShardID)
 		}
 
 		if shard.AverageBlockTxCount != nil {
-			metricsStatistics["erb_shard_stats_average_block_tx_count"].WithLabelValues(node.Name, id).Set(*shard.AverageBlockTxCount)
+			metricsStatistics["erd_shard_stats_average_block_tx_count"].WithLabelValues(node.Name, id).Set(*shard.AverageBlockTxCount)
 		}
 
 		if shard.LastBlockTxCount != nil {
-			metricsStatistics["erb_shard_stats_last_block_tx_count"].WithLabelValues(node.Name, id).Set(*shard.LastBlockTxCount)
+			metricsStatistics["erd_shard_stats_last_block_tx_count"].WithLabelValues(node.Name, id).Set(*shard.LastBlockTxCount)
 		}
 	}
 
 	if statistics.Statistics.LastBlockTxCount != nil {
-		metricsStatistics["erb_stats_last_block_tx_count"].WithLabelValues(node.Name).Set(*statistics.Statistics.LastBlockTxCount)
+		metricsStatistics["erd_stats_last_block_tx_count"].WithLabelValues(node.Name).Set(*statistics.Statistics.LastBlockTxCount)
 	}
 
 	if statistics.Statistics.NrOfShards != nil {
-		metricsStatistics["erb_stats_last_nr_of_shards"].WithLabelValues(node.Name).Set(*statistics.Statistics.NrOfShards)
+		metricsStatistics["erd_stats_last_nr_of_shards"].WithLabelValues(node.Name).Set(*statistics.Statistics.NrOfShards)
 	}
 
 	return nil
--- cmd/elrond-exporter/prometheus.go
+++ cmd/elrond-exporter/prometheus.go
@@ -21,72 +21,72 @@ func initPrometheus() error {
 	metricsStatus = make(map[string]*prometheus.GaugeVec)
 	metricsStatistics = make(map[string]*prometheus.GaugeVec)
 
-	metricsStatistics["erb_stats_live_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_live_tps",
+	metricsStatistics["erd_stats_live_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_live_tps",
 	}, labels)
 
-	metricsStatistics["erb_stats_peak_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_peak_tps",
+	metricsStatistics["erd_stats_peak_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_peak_tps",
 	}, labels)
 
-	metricsStatistics["erb_stats_block_number"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_block_number",
+	metricsStatistics["erd_stats_block_number"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_block_number",
 	}, labels)
 
-	metricsStatistics["erb_stats_round_number"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_round_number",
+	metricsStatistics["erd_stats_round_number"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_round_number",
 	}, labels)
 
-	metricsStatistics["erb_stats_round_time"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_round_time",
+	metricsStatistics["erd_stats_round_time"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_round_time",
 	}, labels)
 
-	metricsStatistics["erb_stats_average_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_average_block_tx_count",
+	metricsStatistics["erd_stats_average_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_average_block_tx_count",
 	}, labels)
 
-	metricsStatistics["erb_stats_total_processed_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_total_processed_tx_count",
+	metricsStatistics["erd_stats_total_processed_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_total_processed_tx_count",
 	}, labels)
 
-	metricsStatistics["erb_shard_stats_live_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_live_tps",
+	metricsStatistics["erd_shard_stats_live_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_live_tps",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_shard_stats_average_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_average_tps",
+	metricsStatistics["erd_shard_stats_average_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_average_tps",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_shard_stats_peak_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_peak_tps",
+	metricsStatistics["erd_shard_stats_peak_tps"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_peak_tps",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_shard_stats_current_block_nonce"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_current_block_nonce",
+	metricsStatistics["erd_shard_stats_current_block_nonce"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_current_block_nonce",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_shard_stats_total_processed_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_total_processed_tx_count",
+	metricsStatistics["erd_shard_stats_total_processed_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_total_processed_tx_count",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_shard_stats_shard_id"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_shard_id",
+	metricsStatistics["erd_shard_stats_shard_id"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_shard_id",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_shard_stats_average_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_average_block_tx_count",
+	metricsStatistics["erd_shard_stats_average_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_average_block_tx_count",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_shard_stats_last_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_shard_stats_last_block_tx_count",
+	metricsStatistics["erd_shard_stats_last_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_shard_stats_last_block_tx_count",
 	}, append(labels, "shard"))
 
-	metricsStatistics["erb_stats_last_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_last_block_tx_count",
+	metricsStatistics["erd_stats_last_block_tx_count"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_last_block_tx_count",
 	}, labels)
 
-	metricsStatistics["erb_stats_last_nr_of_shards"] = factory.NewGaugeVec(prometheus.GaugeOpts{
-		Name: "erb_stats_last_nr_of_shards",
+	metricsStatistics["erd_stats_last_nr_of_shards"] = factory.NewGaugeVec(prometheus.GaugeOpts{
+		Name: "erd_stats_last_nr_of_shards",
 	}, labels)
 
 	return nil
